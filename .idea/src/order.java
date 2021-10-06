565ab204c91e24452212e48fe3e5a01453c17d8b
dariu <vararean04darius@gmail.com>
2021-10-06 12:39:19 +0300

    retusuri

--- .idea/src/order.java
+++ .idea/src/order.java
@@ -2,7 +2,7 @@ public class order
 {
     private static int UID = 1033;
     private static int _dist;
-    private static double _pretsuta=0.0021;
+    private static double _pretmetru=0.0021;
     public static driver sofer;
     public static client cli;
     public order(int dist, driver d, client c)
@@ -14,11 +14,11 @@ public class order
     public static void create_receipt()
     {
         System.out.println("Bon fiscal pentru cursa cu numarul "+ (++UID) + " :");
-        System.out.println("Suma de platit: " + (_dist*_pretsuta) + " RON, pentru distanta: " + _dist );
+        System.out.println("Suma de platit: " + (_dist*_pretmetru) + " RON, pentru distanta: " + _dist );
         System.out.println("Cursa efectuata de soferul: " + sofer.get_nume() + " avand ca pasager pe: " + cli.get_nume());
     }
     public static void cursa()
     {
-        cli.set_bani((cli.get_nrbani()) - (_dist*_pretsuta));
+        cli.set_bani((cli.get_nrbani()) - (_dist*_pretmetru));
     }
 }
